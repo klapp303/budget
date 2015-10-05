62e605bd73896699a4002f749f0cf42213459809
klapp303 <klapp303@gmail.com>
2015-10-05 17:43:03 +0900

    ログイン認証機能を追加

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -31,5 +31,26 @@ App::uses('Controller', 'Controller');
  * @link		http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
 class AppController extends Controller {
-  public $components = array('Session', 'DebugKit.Toolbar');
+  public $components = array(
+      'Session', //Paginateのため記述
+      'Flash', //ここからログイン認証用
+      'Auth' => array(
+          'loginRedirect' => array(
+              'controller' => 'top',
+              'action' => 'index'
+          ),
+          'logoutRedirect' => array(
+              'controller' => 'users',
+              'action' => 'login'
+          ),
+          'authenticate' => array(
+              'Form' => array('passwordHasher' => 'Blowfish')
+          )
+      ),
+      'DebugKit.Toolbar' //ページ右上の開発用デバッグツール
+  );
+
+  /*public function beforeFilter() {
+      $this->Auth->allow('index'); //認証なしのページを設定
+  }*/
 }
--- app/View/Elements/budget_menu.ctp
+++ app/View/Elements/budget_menu.ctp
@@ -2,6 +2,6 @@
     <li><a href="/incomes/">収入管理</a></li>
     <li><a href="/expenditures/">支出管理</a></li>
     <li><a href="/months/">月別収支</a></li>
-    <li><a href="#">収支グラフ</a></li>
-    <li><a href="#">残高グラフ</a></li>
+    <li><a href="#">年間収支</a></li>
+    <li><a href="/users/logout/">ログアウト</a></li>
 </ul>
