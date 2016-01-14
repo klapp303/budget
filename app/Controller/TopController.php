775e2d1091a74fe432f1c6bd3143c533a3ae546a
klapp303 <klapp303@gmail.com>
2016-01-14 16:08:14 +0900

    TOPページでログインユーザのデータのみ表示させる

--- app/Controller/TopController.php
+++ app/Controller/TopController.php
@@ -48,93 +48,126 @@ class TopController extends AppController {
   public function beforeFilter() {
     parent::beforeFilter();
     $this->layout = 'budget_fullwidth';
+    $this->admin_id = 1;
+    $this->set('admin_id', $this->admin_id);
+    $this->set('login_user', $this->Auth->user());
   }
 
   public function index() {
     //現在の残高
-    $income_past_lists = $this->Income->find('list', array(
-        'conditions' => array('Income.date <=' => date('Y-m-d'), 'status' => 1),
-        'fields' => ('Income.amount')
-    ));
-    $this->set('income_past_lists', $income_past_lists);
-    $expenditure_past_lists = $this->Expenditure->find('list', array(
-        'conditions' => array('Expenditure.date <=' => date('Y-m-d'), 'status' => 1),
-        'fields' => ('Expenditure.amount')
-    ));
-    $this->set('expenditure_past_lists', $expenditure_past_lists);
-
-    //次回給料日までの出費
-    if(date('d') < 25) { //1日～24日の場合
-      $expenditure_recent_lists = $this->Expenditure->find('list', array(
-          'conditions' => array(
-              'Expenditure.date >' => date('Y-m-d'),
-              'Expenditure.date <=' => date('Y-m-25')),
+    if ($this->Auth->user('id') == $this->admin_id) { //管理者アカウントの場合
+      $income_past_lists = $this->Income->find('list', array(
+          'conditions' => array('Income.date <=' => date('Y-m-d'), 'Income.status' => 1),
+          'fields' => ('Income.amount')
+      ));
+      $expenditure_past_lists = $this->Expenditure->find('list', array(
+          'conditions' => array('Expenditure.date <=' => date('Y-m-d'), 'Expenditure.status' => 1),
           'fields' => ('Expenditure.amount')
       ));
-    } elseif(date('m') == 12) { //12月25日～31日の場合
-      $expenditure_recent_lists = $this->Expenditure->find('list', array(
-          'conditions' => array(
-              'Expenditure.date >' => date('Y-m-d'),
-              'Expenditure.date <=' => date('Y-1-25', strtotime('+1 year'))),
-          'fields' => ('Expenditure.amount')
+    } else {
+      $income_past_lists = $this->Income->find('list', array(
+          'conditions' => array('Income.date <=' => date('Y-m-d'), 'Income.status' => 1, 'Income.user_id' => $this->Auth->user('id')),
+          'fields' => ('Income.amount')
       ));
-    } else { //25日～31日の場合(12月を除く)
-      $expenditure_recent_lists = $this->Expenditure->find('list', array(
-          'conditions' => array(
-              'Expenditure.date >' => date('Y-m-d'),
-              'Expenditure.date <=' => date('Y-m-25', strtotime('+1 month'))),
+      $expenditure_past_lists = $this->Expenditure->find('list', array(
+          'conditions' => array('Expenditure.date <=' => date('Y-m-d'), 'Expenditure.status' => 1, 'Expenditure.user_id' => $this->Auth->user('id')),
           'fields' => ('Expenditure.amount')
       ));
     }
-    $this->set('expenditure_recent_lists', $expenditure_recent_lists);
+    $this->set('past_income', array_sum($income_past_lists));
+    $this->set('past_expenditure', array_sum($expenditure_past_lists));
+
+    //次回給料日までの出費
+    $pay_date = 25;
+    if(date('d') < $pay_date) { //今月の給料日がまだの場合
+      $expenditure_recent_lists = $this->Expenditure->find('list', array(
+          'conditions' => array(
+              'Expenditure.date >' => date('Y-m-d'),
+              'Expenditure.date <=' => date('Y-m-'.$pay_date)),
+          'fields' => ('Expenditure.amount')
+      ));
+    } else { //今月の給料日を過ぎた場合
+      $expenditure_recent_lists = $this->Expenditure->find('list', array(
+          'conditions' => array(
+              'Expenditure.date >' => date('Y-m-d'),
+              'Expenditure.date <=' => date('Y-m-'.$pay_date, strtotime('+1 month'))),
+          'fields' => ('Expenditure.amount')
+      ));
+    }
+    $this->set('recent_expenditure', array_sum($expenditure_recent_lists));
 
     //確定待ちの収入
-    $income_unfixed_lists = $this->Income->find('all', array(
-        'conditions' => array(
-          'status' => 0,
-          'date <=' => date('Y-m-d')
-        ),
-        'order' => array('date' => 'asc')
-    ));
-//    $this->Paginator->settings = $this->paginate;
-//    $income_unfixed_lists = $this->Paginator->paginate('Income');
-    $income_unfixed_counts = count($income_unfixed_lists);
-//    $this->set('income_unfixed_lists', $income_unfixed_lists);
-    $this->set('income_unfixed_counts', $income_unfixed_counts);
+    if ($this->Auth->user('id') == $this->admin_id) { //管理者アカウントの場合
+      $income_unfixed_lists = $this->Income->find('all', array(
+          'conditions' => array(
+              'Income.status' => 0,
+              'Income.date <=' => date('Y-m-d')
+          ),
+          'order' => array('Income.date' => 'asc')
+      ));
+    } else {
+      $income_unfixed_lists = $this->Income->find('all', array(
+          'conditions' => array(
+              'Income.status' => 0,
+              'Income.date <=' => date('Y-m-d'),
+              'Income.user_id' => $this->Auth->user('id')
+          ),
+          'order' => array('Income.date' => 'asc')
+      ));
+    }
+    $this->set('income_unfixed_counts', count($income_unfixed_lists));
 
     //確定待ちの支出
-    $expenditure_unfixed_lists = $this->Expenditure->find('all', array(
-        'conditions' => array(
-          'status' => 0,
-          'date <=' => date('Y-m-d')
-        ),
-        'order' => array('date' => 'asc')
-    ));
-//    $this->Paginator->settings = $this->paginate;
-//    $expenditure_unfixed_lists = $this->Paginator->paginate('Expenditure');
-    $expenditure_unfixed_counts = count($expenditure_unfixed_lists);
-//    $this->set('expenditure_unfixed_lists', $expenditure_unfixed_lists);
-    $this->set('expenditure_unfixed_counts', $expenditure_unfixed_counts);
+    if ($this->Auth->user('id') == $this->admin_id) { //管理者アカウントの場合
+      $expenditure_unfixed_lists = $this->Expenditure->find('all', array(
+          'conditions' => array(
+              'Expenditure.status' => 0,
+              'Expenditure.date <=' => date('Y-m-d')
+          ),
+          'order' => array('Expenditure.date' => 'asc')
+      ));
+    } else {
+      $expenditure_unfixed_lists = $this->Expenditure->find('all', array(
+          'conditions' => array(
+              'Expenditure.status' => 0,
+              'Expenditure.date <=' => date('Y-m-d'),
+              'Expenditure.user_id' => $this->Auth->user('id')
+          ),
+          'order' => array('Expenditure.date' => 'asc')
+      ));
+    }
+    $this->set('expenditure_unfixed_counts', count($expenditure_unfixed_lists));
 
     //本日の支出
     $expenditure_now_lists = $this->Expenditure->find('all', array(
         'conditions' => array(
-            'Expenditure.date' => date('Y-m-d')),
-        'order' => array('date' => 'asc')
+            'Expenditure.date' => date('Y-m-d'),
+            'Expenditure.user_id' => $this->Auth->user('id')
+        ),
+        'order' => array('Expenditure.date' => 'asc')
     ));
     $expenditure_now_counts = count($expenditure_now_lists);
-    $this->set('expenditure_now_lists', $expenditure_now_lists);
-    $this->set('expenditure_now_counts', $expenditure_now_counts);
+    $this->set(compact('expenditure_now_lists', 'expenditure_now_counts'));
 
     //直近の支出
-    $expenditure_month_lists = $this->Expenditure->find('all', array(
-        'conditions' => array(
-            'Expenditure.date >' => date('Y-m-d'),
-            'Expenditure.date <=' => date('Y-m-d', strtotime('+1 month'))),
-        'order' => array('date' => 'asc')
-    ));
-    $expenditure_month_counts = count($expenditure_month_lists);
+    if ($this->Auth->user('id') == $this->admin_id) { //管理者アカウントの場合
+      $expenditure_month_lists = $this->Expenditure->find('all', array(
+          'conditions' => array(
+              'Expenditure.date >' => date('Y-m-d'),
+              'Expenditure.date <=' => date('Y-m-d', strtotime('+1 month'))
+          ),
+          'order' => array('Expenditure.date' => 'asc')
+      ));
+    } else {
+      $expenditure_month_lists = $this->Expenditure->find('all', array(
+          'conditions' => array(
+              'Expenditure.date >' => date('Y-m-d'),
+              'Expenditure.date <=' => date('Y-m-d', strtotime('+1 month')),
+              'Expenditure.user_id' => $this->Auth->user('id')
+          ),
+          'order' => array('Expenditure.date' => 'asc')
+      ));
+    }
     $this->set('expenditure_month_lists', $expenditure_month_lists);
-    $this->set('expenditure_month_counts', $expenditure_month_counts);
   }
 }
--- app/View/Top/index.ctp
+++ app/View/Top/index.ctp
@@ -1,20 +1,26 @@
 <h3><?php echo date('Y年m月d日'); ?>現在の収支情報</h3>
 
-  <?php $past_income = array_sum($income_past_lists);
-        $past_expenditure = array_sum($expenditure_past_lists); ?>
-<table class="fl cf">
-    <tr><td>現在の残高</td><td class="tbl-num"><?php echo $past_income - $past_expenditure; ?>円</td></tr>
-    <tr><td>次回給料日までの出費</td><td class="tbl-num"><?php echo array_sum($expenditure_recent_lists); ?>円</td></tr>
+  <?php if ($login_user['id'] == $admin_id) { //管理者アカウントの場合 ?>
+  <table class="fl cf">
+    <tr><td>確定済みの収入</td><td class="tbl-num"><?php echo $past_income; ?>円</td></tr>
+    <tr><td>確定済みの支出</td><td class="tbl-num"><?php echo $past_expenditure; ?>円</td></tr>
   </table>
+  <?php } else { ?>
+  <table class="fl cf">
+    <tr><td>現在の残高</td><td class="tbl-num"><?php echo $past_income - $past_expenditure; ?>円</td></tr>
+    <tr><td>次回給料日までの出費</td><td class="tbl-num"><?php echo $recent_expenditure; ?>円</td></tr>
+  </table>
+  <?php } ?>
 
   <table class="tbl-budget_top">
     <tr><td>確定待ちの収入</td><td><?php echo $this->Html->link($income_unfixed_counts.'件', '/incomes/fix/'); ?></td></tr>
     <tr><td>確定待ちの支出</td><td><?php echo $this->Html->link($expenditure_unfixed_counts.'件', '/expenditures/fix/'); ?></td></tr>
   </table>
 
+<?php if ($login_user['id'] != $admin_id) { //管理者アカウント以外の場合 ?>
 <h3 class="title-ex-now_top">本日の支出</h3>
 
-  <?php if($expenditure_now_counts > 0) { ?>
+  <?php if ($expenditure_now_counts > 0) { ?>
     <table class="detail-list">
       <tr><th>日付</th><th>支出名</th><th class="tbl-num">金額</th><th class="tbl-ico">種類</th><th class="tbl-ico">状態</th></tr>
       <?php for($i = 0; $i < $expenditure_now_counts; $i++){ ?>
@@ -29,17 +35,18 @@
   <?php } else { ?>
     <p>本日の支出はありません。</p>
   <?php } ?>
+<?php } ?>
 
-<h3>直近の支出</h3>
+<h3>直近の支出予定</h3>
 
   <table class="detail-list">
     <tr><th>日付</th><th>支出名</th><th class="tbl-num">金額</th><th class="tbl-ico">種類</th><th class="tbl-ico">状態</th></tr>
-    <?php for($i = 0; $i < $expenditure_month_counts; $i++){ ?>
-    <tr><td><?php echo $expenditure_month_lists[$i]['Expenditure']['date']; ?></td>
-        <td><?php echo $expenditure_month_lists[$i]['Expenditure']['title']; ?></td>
-        <td class="tbl-num"><?php echo $expenditure_month_lists[$i]['Expenditure']['amount']; ?>円</td>
-        <td class="tbl-ico"><span class="icon-genre col-e_<?php echo $expenditure_month_lists[$i]['Expenditure']['genre_id']; ?>"><?php echo $expenditure_month_lists[$i]['ExpendituresGenre']['title']; ?></span></td>
-        <td class="tbl-ico"><?php if ($expenditure_month_lists[$i]['Expenditure']['status'] == 0) {echo '<span class="icon-false">未定</span>';}
-                              elseif ($expenditure_month_lists[$i]['Expenditure']['status'] == 1) {echo '<span class="icon-true">確定</span>';} ?></td></tr>
+    <?php foreach($expenditure_month_lists AS $expenditure_month_list){ ?>
+    <tr><td><?php echo $expenditure_month_list['Expenditure']['date']; ?></td>
+        <td><?php echo $expenditure_month_list['Expenditure']['title']; ?></td>
+        <td class="tbl-num"><?php echo $expenditure_month_list['Expenditure']['amount']; ?>円</td>
+        <td class="tbl-ico"><span class="icon-genre col-e_<?php echo $expenditure_month_list['Expenditure']['genre_id']; ?>"><?php echo $expenditure_month_list['ExpendituresGenre']['title']; ?></span></td>
+        <td class="tbl-ico"><?php if ($expenditure_month_list['Expenditure']['status'] == 0) {echo '<span class="icon-false">未定</span>';}
+                              elseif ($expenditure_month_list['Expenditure']['status'] == 1) {echo '<span class="icon-true">確定</span>';} ?></td></tr>
     <?php } ?>
   </table>
